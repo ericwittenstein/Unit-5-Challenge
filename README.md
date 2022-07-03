1abdcbaa8266021ac78ac47b8ef256af8fcb2fb1
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-07-03 18:15:59 -0400

    push for deployment and functionality testing

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Having read the instructions, I have, to the best of my abilities, done the following:
 
 - modified the provided files so that a daily agenda is dispalyed in one-hour increments, with 1 task savable per hour
-- created a JavaScript file to handle the various dynamic elements of the page, including color of agenda box (gray for past, red for present, green for future); saving and retrieving from local storage; displaying the date; etc.
+- created a JavaScript file to handle the various dynamic elements of the page, including color of agenda box (gray for past, red for present, green for future); saving tasks entered to a local storage object; retrieving from local storage on page load
 - added screenshots and links to this readme for the deployed application repo
 - pushed my files to GitHub and deployed them to a live site through GitHub
 - submitted the deployed application URL and GitHub repo to BCS for review and grading
--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,3 @@
-// array of objects to store hour value (for past/present/future color checking), time to display, and index. can moment take a 1pm and interpret it as 1300? answer: no. solution: one variable to count hours military style, one to display the time to the page
-/* var todayAgenda = [
-    {
-        indexId: "0",
-        hourOfTheDay: "09",
-        timeToDisplay: "9am",
-        task: ""
-    },
-    {
-        indexId: "1",
-        hourOfTheDay: "10",
-        timeToDisplay: "10am",
-        task: ""
-    },
-    {
-        indexId: "2",
-        hourOfTheDay: "11",
-        timeToDisplay: "11am",
-        task: ""
-    },
-    {
-        indexId: "3",
-        hourOfTheDay: "12",
-        timeToDisplay: "12pm",
-        task: ""
-    },
-    {
-        indexId: "4",
-        hourOfTheDay: "13",
-        timeToDisplay: "1pm",
-        task: ""
-    },
-    {
-        indexId: "5",
-        hourOfTheDay: "14",
-        timeToDisplay: "2pm",
-        task: ""
-    },
-    {
-        indexId: "6",
-        hourOfTheDay: "15",
-        timeToDisplay: "3pm",
-        task: ""
-    },
-    {
-        indexId: "7",
-        hourOfTheDay: "16",
-        timeToDisplay: "4pm",
-        task: ""
-    },
-    {
-        indexId: "8",
-        hourOfTheDay: "17",
-        timeToDisplay: "5pm",
-        task: ""
-    },
-]*/
-
-// pulled this function from a stack answer after googling how to save to local storage and this seemed like the simplest answer. basically takes the value of the day array items and turns them into strings
-// function saveAgenda() {
-//     localStorage.setItem("Today's Agenda", JSON.stringify(todayAgenda));
-// }
-
-// this function *should* pull info from local storage and display it to the calendar. not sure how to run through a local storage array though, gonna check with tutoring for help
-// function displayAgenda() {
-//     todayAgenda.forEach(function (eachHour) {
-//     });
-// }
-
-// EVERYTHING ABOVE THIS LINE CAN BE DELETED IF EVERYTHING BELOW THIS LINE WORKS
-
 // displays the date at the top of the page
 function displayDate() {
     var todaysDate = moment();
@@ -106,3 +35,23 @@ $(".saveBtn").on("click", function(event) {
 // sets the interval to 1 IRL sec for the date display, box coloring, 
 setInterval(displayDate, 1000);
 setInterval(boxColor, 1000);
+
+// function to pull items from local storage
+function updateTasks () {
+    // for each column element, which targets the text box area
+    $(".col").each(function () {
+        // get the current hour id
+        var hourToUpdate = $(this).attr("id");
+        // get the local storage item at that key, using the id as the key
+        var taskToUpdate = localStorage.getItem(hourToUpdate);
+        // if there is nothing there, it returns a null, so as long as something is there, set the current text box area to the value pulled from local storage
+        if (taskToUpdate != null) {
+            $(this).text(taskToUpdate);
+        }
+    })
+}
+
+// function to run on page ready
+$(document).ready(function() {
+    updateTasks();
+})
